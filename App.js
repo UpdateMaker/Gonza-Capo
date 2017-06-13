612f73fe76f7c5fdcc2139eac84e8dd39e112b7e
UpdateMaker <jiveron109@gmail.com>
2017-06-13 12:38:51 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        Gonza Capo
+// @include     *//www.google.com.*
+// @version     1
+// @grant       GM_xmlhttpRequest
+// ==/UserScript==
+
+
+var input=document.createElement("input");
+input.type="button";
+  GM_xmlhttpRequest({
+     method: "GET",
+     url: "http://ip-api.com/json",
+     onload: function(xhr) {
+      var data = eval("(" + xhr.responseText + ")");
+     
+         input.value = "Tu ISP es " + data['isp'];
+         input.onclick = showAlert;
+
+input.setAttribute("style", "font-size:12px;position:absolute;top:20px;right:320px;");
+document.body.appendChild(input); 
+ 
+function showAlert()
+{
+    alert("Localización: " + data['city'] + ", " + data['country'] + "\n" + "IP: " + data['query'] + "\n");
+}
+    }
+  });
+
